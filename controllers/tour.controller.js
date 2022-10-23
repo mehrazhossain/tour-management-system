13f7dcb9a0dfadba3538f4ee329b613083079fbe
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-10-23 12:21:50 +0600

    Pagination added

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -3,6 +3,49 @@ const {
   getToursService,
 } = require('../services/tour.services');
 
+exports.getTours = async (req, res, next) => {
+  try {
+    let filters = { ...req.query };
+
+    // exclude
+    const excludeFields = ['sort', 'page', 'limit'];
+    excludeFields.forEach((field) => delete filters[field]);
+
+    const queries = {};
+    console.log('before queries', queries);
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      queries.sortBy = sortBy;
+    }
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      queries.fields = fields;
+    }
+
+    if (req.query.page) {
+      const { page = 1, limit = 10 } = req.query;
+      const skip = (page - 1) * parseInt(limit);
+      queries.skip = skip;
+      queries.limit = parseInt(limit);
+    }
+    console.log('after queries', queries);
+
+    const tours = await getToursService(filters, queries);
+
+    res.status(200).json({
+      status: 'success',
+      data: tours,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: "Can't get the data",
+      error: error.message,
+    });
+  }
+};
+
 exports.createTour = async (req, res, next) => {
   try {
     const result = await createTourService(req.body);
@@ -16,23 +59,6 @@ exports.createTour = async (req, res, next) => {
     res.status(400).json({
       status: 'fail',
       message: 'Tour is not inserted',
-      error: error.message,
-    });
-  }
-};
-
-exports.getTours = async (req, res, next) => {
-  try {
-    const tours = await getToursService();
-
-    res.status(200).json({
-      status: 'success',
-      data: tours,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: "Can't get the data",
       error: error.message,
     });
   }
--- services/tour.services.js
+++ services/tour.services.js
@@ -1,8 +1,16 @@
 const Tour = require('../models/Tour');
 
-exports.getToursService = async () => {
-  const tours = await Tour.find({});
-  return tours;
+exports.getToursService = async (filters, queries) => {
+  const tours = await Tour.find(filters)
+    .skip(queries.skip)
+    .limit(queries.limit)
+    .select(queries.fields)
+    .sort(queries.sortBy);
+
+  const totalTours = await Tour.countDocuments(filters);
+  const pageCount = Math.ceil(totalTours / queries.limit);
+
+  return { totalTours, pageCount, tours };
 };
 
 exports.createTourService = async (data) => {
